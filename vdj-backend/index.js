ebf8fde3515b3e999cbb2f213809664f64d3d8e5
VimzyCoolz <vimzycoolz@example.com>
2026-05-30 16:04:26 +0300

    Removed dotenv.config in backend to rely on live server environment variables

--- vdj-backend/index.js
+++ vdj-backend/index.js
@@ -156,7 +156,7 @@ if (typeof BigInt !== 'undefined') {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
